b6c0a4c807563054abb55312bd238fe13114aff9
Paweł Kęcerski <pkecerski@icloud.com>
2021-03-16 20:25:05 +0100

    v0.9

--- mirrorstring.cpp
+++ mirrorstring.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+string mirrorstring(string &sIn);
+string sInput;
+
+int main(){
+    printf("Please insert your string: ");
+    getline(cin, sInput);
+    printf("You entered: %s\n", sInput.c_str());
+    cout << "The reverse of that is: " << mirrorstring(sInput) << endl;
+}
+
+string mirrorstring(string &sIn){
+    char sResult[sIn.length()];
+    string sOut;
+
+    for(int i=0; i<=sIn.length(); i++){
+        sOut += sIn[sIn.length()-i];
+    }
+    return sOut;
+}
